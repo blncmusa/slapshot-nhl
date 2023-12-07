76d750ce15c434fbfd0b0d25f25065382376be97
Musa <97222228+blncmusa@users.noreply.github.com>
2023-12-07 00:51:25 +0000

    renamed directory & repo

---
+++ slapshot-nhl/components/HorizontalSplit.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default function HorizontalSplit(){
+    return (
+        <>
+         <View style={styles.horizontalContainer}>
+            <View style={styles.horizontalSplit}></View>
+         </View>
+    </>
+    )
+}
+
+const styles = StyleSheet.create({
+    horizontalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        width: wp("100%"),
+        alignItems: "center"
+    },
+    horizontalSplit: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#000",
+        marginVertical: 20,
+        width: wp("80%")
+    }
+})
---
+++ slapshot-nhl/screens/StatsScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView, View, Text } from "react-native";
+
+export default function StatsScreen(){
+    return(
+        <>
+            <View>
+                <Text>Stats Page!</Text>
+            </View>
+        </>
+    )
+}
